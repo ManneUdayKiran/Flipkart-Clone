6f9c67515bb7b3bcb49639ef1cabcadd2e9818a5
MANNE UDAY KIRAN <143863214+ManneUdayKiran@users.noreply.github.com>
2025-06-21 16:42:58 +0530

    Update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -5,7 +5,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
-    port: parseInt(process.env.PORT) || 5173
+    port: parseInt(process.env.PORT) || 5173,
+     allowedHosts: ['flipkart-clone-gv4r.onrender.com']
    
   }
 });
